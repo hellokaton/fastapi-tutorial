e787d6136326420949df2d0ec94044421601f97f
biezhi <biezhi.me@gmail.com>
2021-11-23 17:03:42 +0800

    :sparkles: add oauth2 username password example

--- 13_oauth2_username_password/main.py
+++ 13_oauth2_username_password/main.py
@@ -1,24 +1,93 @@
-from typing import Optional, List
+from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI, Header, Cookie
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+fake_users_db = {
+    "biezhi": {
+        "username": "biezhi",
+        "email": "biezhi.me@gmail.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 app = FastAPI()
 
 
-@app.get("/read_header", summary="获取 header 参数")
-async def read_header(user_agent: Optional[str] = Header(None)):
-    return {"User-Agent": user_agent}
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
 
-@app.get("/header_list", summary="重复的 header")
-async def header_list(x_token: Optional[List[str]] = Header(None)):
-    return {"X-Token values": x_token}
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@app.get("/read_cookie", summary="获取 cookie 参数")
-async def read_cookie(cc_id: Optional[str] = Cookie(None)):
-    return {"cc_id": cc_id}
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="无效的认证凭据",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="不存在该用户")
+    return current_user
+
+
+@app.post("/token", summary="用户名密码认证获取Token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="用户名或密码错误")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="用户名或密码错误")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me", summary="访问认证后资源")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', reload=True, port=9005)
+    uvicorn.run('main:app', host='127.0.0.1', reload=True, port=9000)
