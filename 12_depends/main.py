6842acd23e084c29c6d0e090be9a24b3026b5e3e
biezhi <biezhi.me@gmail.com>
2021-11-23 16:50:39 +0800

    :sparkles: add depends example

--- 12_depends/main.py
+++ 12_depends/main.py
@@ -1,9 +1,16 @@
 from typing import Optional
 
 import uvicorn
-from fastapi import Depends, FastAPI, Query
+from fastapi import Depends, FastAPI, Query, HTTPException, Header, Request
 
-app = FastAPI()
+
+# 全局依赖项
+async def request_log(request: Request):
+    print('请求路径:', request.url)
+    print('请求方法:', request.method)
+
+
+app = FastAPI(dependencies=[Depends(request_log)])
 
 
 async def common_parameters(q: str = Query(None, description="查询关键词"),
@@ -12,12 +19,12 @@ async def common_parameters(q: str = Query(None, description="查询关键词"),
     return {"q": q, "page": page, "limit": limit}
 
 
-@app.get("/items")
+@app.get("/read_items")
 async def read_items(commons: dict = Depends(common_parameters)):
     return commons
 
 
-@app.get("/users")
+@app.get("/read_users")
 async def read_users(commons: dict = Depends(common_parameters)):
     return commons
 
@@ -44,6 +51,22 @@ async def read_items(commons: CommonQueryParams = Depends(CommonQueryParams)):
     items = fake_items_db[commons.skip: commons.skip + commons.limit]
     response.update({"items": items})
     return response
+
+
+async def verify_token(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_key(x_key: str = Header(...)):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+
+@app.get("/users_depends", summary="路径依赖项", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def users():
+    return [{"username": "张三"}, {"username": "罗翔"}]
 
 
 if __name__ == '__main__':
