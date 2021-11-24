9cab341e4ff75d88aa8f828f284e8ad7b8774b05
biezhi <biezhi.me@gmail.com>
2021-11-24 10:24:48 +0800

    :sparkles: add websocket example

--- 20_websocket/main.py
+++ 20_websocket/main.py
@@ -1,45 +1,43 @@
+from typing import List
+
 import uvicorn
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
 
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>发送</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        
-        <script>
-            var ws = new WebSocket("ws://127.0.0.1:9000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
+class ConnectionManager:
+    def __init__(self):
+        # 存放激活的ws连接对象
+        self.active_connections: List[WebSocket] = []
 
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+    async def connect(self, ws: WebSocket):
+        # 等待连接
+        await ws.accept()
+        # 存储ws连接对象
+        self.active_connections.append(ws)
+
+    def disconnect(self, ws: WebSocket):
+        # 关闭时 移除ws对象
+        self.active_connections.remove(ws)
+
+    @staticmethod
+    async def send_personal_message(message: str, ws: WebSocket):
+        # 发送个人消息
+        await ws.send_text(message)
+
+    async def broadcast(self, message: str):
+        # 广播消息
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+html = ""
+with open('chat.html', 'r') as f:
+    html = f.read()
 
 
 @app.get("/")
@@ -47,12 +45,20 @@ async def get():
     return HTMLResponse(html)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+@app.websocket("/ws/{user}")
+async def websocket_endpoint(websocket: WebSocket, user: str):
+    await manager.connect(websocket)
+    await manager.broadcast(f"用户{user}进入聊天室")
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"你说了: {data}", websocket)
+            await manager.broadcast(f"用户:{user} 说: {data}")
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"用户-{user}-离开")
 
 
 if __name__ == '__main__':
